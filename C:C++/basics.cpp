58981c99234e919d2ab6e6384da47e36da18cd79
RHS_Air <rhs_air@MacBook-Air.local>
2020-02-19 19:20:54 -0500

    Basics

---
+++ C:C++/basics.cpp
@@ -0,0 +1,23 @@
+/*
+Written By RSimon for the AUC Code Club
+*/
+
+// Basic of c++ //
+
+// Variables
+
+int x = 2; // Integers, stores integer values
+char a = 'A'; // Character, stores a single letter or character
+bool val = true; // Boolean, stores a truth value, either true or false
+float y = 3.141592659; // Floating point number, generally used to store decimals
+
+// Math Operators
+
+int add = 3 + 2; // Addition
+int sub = 5 - 3; // Subtraction
+int divide = 6 / 4; // Division, this returns a rounded integer
+float divide_2 = 6.0 / 5; // Division, this returns a floating point number
+int mod = 4 % 3; // Modulo, returns the remainder of the two numbers when divided
+
+// Main Function 
+int main() {}
